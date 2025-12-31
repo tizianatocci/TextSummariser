b0a41dc62059935e64a2f261c7ffbaf70200b4de
tizianatocci <tiziana.tocci96@hotmail.it>
2025-12-31 17:15:45 +0100

    data ingestion completed

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from src.TextSummarizer.logging import logger
+from src.TextSummarizer.pipeline.stage_1_data_ingestion_pipeline import DataIngestionTrainingPipeline
 
-logger.info("Logging is implemented")
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f"Stage {STAGE_NAME} initiated")
+    data_ingestion_pipeline = DataIngestionTrainingPipeline()
+    data_ingestion_pipeline.initiate_data_ingestion()
+    logger.info(f"Stage {STAGE_NAME} completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
