3d965465fb2fc120fd1044b96beaff7bf8be716d
tizianatocci <tiziana.tocci96@hotmail.it>
2026-01-23 17:58:39 +0100

    app modified

--- app.py
+++ app.py
@@ -5,4 +5,22 @@ import os
 from fastapi.templating import Jinja2Templates
 from starlette.responses import RedirectResponse
 from fastapi.responses import Response
-from TextSummarizer.pipeline.prediction import PredictionPipeline
+from src.TextSummarizer.pipeline.prediction import PredictionPipeline
+
+txt:str = "What is Text Summarization?"
+
+app = FastAPI()
+
+@app.get("/", tags = ["authentication"])
+async def index():
+    return RedirectResponse(url="/docs")
+
+@app.get("/train")
+async def training():
+    try:
+        os.system("python main.py")
+        return Response("Training successful!!")
+    except Exception as e:
+        return Response(f"Error occurred! {e}")
+    
+    
